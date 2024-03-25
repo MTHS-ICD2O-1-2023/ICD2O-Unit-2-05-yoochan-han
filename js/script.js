da048a8ba903f8f46255b58fb3836bf0b2e9b0f2
yoochan han <159185007+yoochan-han@users.noreply.github.com>
2024-03-25 18:32:03 +0000

    added html, css, js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,16 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: yoochan 
+// Created on: Mar 2024
 // This file contains the JS functions for index.html
+function CaculatePay() {
+  const wt = parseFloat(document.getElementById("get_wt").value)
+  const rate = parseFloat(document.getElementById("get_salary").value)
+  const taxRate = parseFloat(0.18)
+  // caculate
+  const pay = (wt*rate)*(1.00-0.18)
+  const taxes = (wt * rate) * 0.18
+  // display
+  document.getElementById("UrPay").innerHTML = 'Your pay will be: $' + pay.toFixed(2)
+  document.getElementById("Tax").innerHTML = 'Govenment will take: $' + taxes.toFixed(2)
+}
