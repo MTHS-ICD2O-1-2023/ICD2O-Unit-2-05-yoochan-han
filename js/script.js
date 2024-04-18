bbeae1f22615c7d35f201479577062c9f7c26062
yoochan han <159185007+yoochan-han@users.noreply.github.com>
2024-04-18 17:28:45 +0000

    debug

--- js/script.js
+++ js/script.js
@@ -1,15 +1,15 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
 // Created by: yoochan 
-// Created on: Mar 2024
+// Created on: Mar, 2024 (edited on: Apr 18, 2024)
 // This file contains the JS functions for index.html
 function CaculatePay() {
-  const wt = parseFloat(document.getElementById("get_wt").value)
+  const workTime = parseFloat(document.getElementById("get_workTime").value)
   const rate = parseFloat(document.getElementById("get_salary").value)
   const taxRate = parseFloat(0.18)
   // caculate
-  const pay = (wt*rate)*(1.00-0.18)
-  const taxes = (wt * rate) * 0.18
+  const pay = (workTime*rate)*(1.00-taxRate)
+  const taxes = (workTime * rate) * taxRate
   // display
   document.getElementById("UrPay").innerHTML = 'Your pay will be: $' + pay.toFixed(2)
   document.getElementById("Tax").innerHTML = 'Govenment will take: $' + taxes.toFixed(2)
